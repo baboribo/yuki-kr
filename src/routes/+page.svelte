8c7534020967206dd245830d67bc6c25d5db40f9
baboribo <inertia_mailer_0c@icloud.com>
2026-03-07 02:00:37 +0900

    feat: implement dynamic name toggle in main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>yuki</h1>
+<script>
+    let name = "yuki";
+    let toggle = false;
+
+    import { onMount } from "svelte";
+    onMount(() => {
+        setInterval(() => {
+            toggle = !toggle;
+            name = toggle ? "yuki" : "yuki!";
+        }, 500);
+    });
+</script>
+
+<h1>{name}</h1>
 <p>yuki.kr</p>
