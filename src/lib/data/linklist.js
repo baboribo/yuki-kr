073947a2424dfce2ff20bc9b0b8a586daa981255
baboribo <inertia_mailer_0c@icloud.com>
2026-03-15 14:42:07 +0900

    fix: maile 링크에 https 누락된 부분 해결

--- src/lib/data/linklist.js
+++ src/lib/data/linklist.js
@@ -12,6 +12,6 @@ export const links = [
     {
         id: 3,
         name: "Maile",
-        url: "mailnote.vercel.app" // 임시 주소이니 나중에 도메인 연결 후 변경해야함.
+        url: "https://mailnote.vercel.app" // 임시 주소이니 나중에 도메인 연결 후 변경해야함.
     }
 ];
